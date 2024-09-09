5d5963392ce6791775bd7689e38f36b50edeb4ac
Roney Fábio <roneyfabio@gmail.com>
2024-09-09 09:22:30 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+      titulo: "Rayssa Leal",
+      descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento precoce. Aos poucos anos, ela se tornou um ícone do esporte e inspiração para jovens skatistas.",
+      link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+      tags: "skate fada sk8"
+    },
+
+    {
+      titulo: "Beatriz Souza",
+      descricao: "Beatriz Souza é uma judoca brasileira que conquistou o país com suas vitórias nas competições internacionais. Sua força e determinação a tornaram uma das maiores promessas do judô brasileiro.",
+      link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+      tags: "judô judo judoka"
+    },
+
+    {
+      titulo: "Rebeca Andrade",
+      descricao: "Rebeca Andrade é uma das maiores ginastas da história do Brasil. Com seu talento e determinação, ela conquistou o coração dos brasileiros e se tornou um ícone do esporte nacional.",
+      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+      tags: "ginástica ginastica ouro"
+    }
+  ];
+
